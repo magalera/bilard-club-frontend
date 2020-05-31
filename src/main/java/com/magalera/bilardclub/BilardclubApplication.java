8bdbe1cf9b79d69af028663d2116d6a43bb69e6d
Małgorzata Galera <malgorzata.galera.92@gmail.com>
2020-05-31 14:12:20 +0200

    Initial commit

---
+++ src/main/java/com/magalera/bilardclub/BilardclubApplication.java
@@ -0,0 +1,13 @@
+package com.magalera.bilardclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilardclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilardclubApplication.class, args);
+    }
+
+}
